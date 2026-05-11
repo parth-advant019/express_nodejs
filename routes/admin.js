b0273c9cf9dd9dba5550e503fd19add3eb8ca557
parth <parth@advant.xyz>
2026-05-11 19:35:37 +0530

    add ejs

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,23 @@ const rootDir = require("../utils/path");
 
 const router = express.Router();
 
+const products = [];
+
 // rich /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Products",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
 });
 
 router.post("/add-product", (req, res) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,20 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
