a4d7e1b68e2267487f3ef33a4e67e64ec2278d95
parth <parth@advant.xyz>
2026-05-12 11:11:25 +0530

    add file fetch code

--- models/product.js
+++ models/product.js
@@ -1,22 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "product.json",
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "product.json",
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,17 +31,7 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "product.json",
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return [];
-      }
-      return JSON.parse(fileContent);
-    });
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
