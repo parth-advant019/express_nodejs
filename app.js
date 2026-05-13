6a5c28e6b5ee07568fd6e5c525e3bc3351b944c7
parth <parth@advant.xyz>
2026-05-13 22:56:27 +0530

    connect workbench

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const db = require("./utils/database");
 
 const app = express();
 
 app.set("view engine", "ejs");
-//app.set("view engine", "pug");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
